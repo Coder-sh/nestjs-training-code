e292708f5283bd2bc115d523e35b9778c700e2fa
hengsong <heng.song@fenrir-inc.com.cn>
2023-09-25 12:25:33 +0800

    feat: all-decorator

--- all-decorator/src/aaa.controller.ts
+++ all-decorator/src/aaa.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, HostParam, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Controller,
+  Get,
+  Header,
+  HostParam,
+  HttpCode,
+  Next,
+  Redirect,
+  Render,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Controller({ host: ':host.0.0.1', path: 'aaa' })
 export class AaaController {
@@ -17,6 +28,41 @@ export class AaaController {
   @Get('ddd')
   ddd(@Res({ passthrough: true }) res: Response) {
     return 'ddd';
-    res.send('ddd');
+    // res.send('ddd');
+  }
+
+  @Get('eee')
+  eee(@Next() next: NextFunction) {
+    console.log('handler1');
+    next();
+    return '111';
+  }
+
+  @Get('eee')
+  eee2() {
+    console.log('handler2');
+    return 'eee';
+  }
+
+  @Get('fff')
+  @HttpCode(222)
+  fff() {
+    return 'hello';
+  }
+
+  @Get('ggg')
+  @Header('aaa', 'bbb')
+  ggg() {
+    return 'hello';
+  }
+
+  @Get('hhh')
+  @Redirect('http://juejin.cn')
+  hhh() {}
+
+  @Get('user')
+  @Render('user')
+  user() {
+    return { name: 'guang', age: 20 };
   }
 }
