9eaac93c4949d8e62257088fece062f9b03f15bc
hengsong <heng.song@fenrir-inc.com.cn>
2023-09-27 17:15:39 +0800

    feat: dynamic-module

---
+++ dynamic-module/src/ccc/ccc.module-definition.ts
@@ -0,0 +1,18 @@
+import { ConfigurableModuleBuilder } from '@nestjs/common';
+
+export interface CccModuleOptions {
+  aaa: number;
+  bbb: string;
+}
+
+export const {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN,
+  OPTIONS_TYPE,
+  ASYNC_OPTIONS_TYPE,
+} = new ConfigurableModuleBuilder<CccModuleOptions>()
+  .setExtras({ isGlobal: true }, (definition, extras) => ({
+    ...definition,
+    global: extras.isGlobal,
+  }))
+  .build();
