a76c29d0b00d5cc641d58cf3c223696f15e06e8b
hengsong <heng.song@fenrir-inc.com.cn>
2023-10-13 18:03:21 +0800

    feat: pipe

---
+++ pipe-test/src/app.controller.ts
@@ -0,0 +1,114 @@
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseArrayPipe,
+  ParseBoolPipe,
+  ParseEnumPipe,
+  ParseFloatPipe,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { AaaPipe } from './aaa.pipe';
+
+enum Ggg {
+  AAA = '111',
+  BBB = '222',
+  CCC = '333',
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(
+    @Query(
+      'aa',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_FOUND,
+      }),
+    )
+    aa: string,
+  ): string {
+    return aa + 1;
+  }
+
+  @Get('bb')
+  bb(
+    @Query(
+      'aa',
+      new ParseIntPipe({
+        exceptionFactory: (msg) => {
+          console.log(msg);
+          throw new HttpException('xxx' + msg, HttpStatus.NOT_IMPLEMENTED);
+        },
+      }),
+    )
+    aa: string,
+  ): string {
+    return aa + 1;
+  }
+
+  @Get('cc')
+  cc(@Query('cc', ParseFloatPipe) cc: number) {
+    return cc + 1;
+  }
+
+  @Get('dd')
+  dd(@Query('dd', ParseBoolPipe) dd: boolean) {
+    return typeof dd;
+  }
+
+  @Get('ee')
+  ee(
+    @Query(
+      'ee',
+      new ParseArrayPipe({
+        items: Number,
+      }),
+    )
+    ee: Array<number>,
+  ) {
+    return ee.reduce((total, item) => total + item, 0);
+  }
+
+  @Get('ff')
+  ff(
+    @Query(
+      'ff',
+      new ParseArrayPipe({
+        separator: '..',
+        optional: true,
+      }),
+    )
+    ff: Array<string>,
+  ) {
+    return ff;
+  }
+
+  @Get('gg/:enum')
+  gg(@Param('enum', new ParseEnumPipe(Ggg)) e: Ggg) {
+    return e;
+  }
+
+  @Get('hh/:uuid')
+  hh(@Param('uuid', new ParseUUIDPipe()) uuid: string) {
+    return uuid;
+  }
+
+  @Get('kkk')
+  kkk(@Query('kkk', new DefaultValuePipe('aaa')) kkk: string) {
+    return kkk;
+  }
+
+  @Get('nnn/:bbb')
+  nnn(@Query('aaa', AaaPipe) aaa: string, @Param('bbb', AaaPipe) bbb: number) {
+    return aaa + bbb;
+  }
+}
